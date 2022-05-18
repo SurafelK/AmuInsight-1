f76fc8a6f9d8cc8b09363d059e371a3e23e592df
feshfaa <feshfaa@gmail.com>
2022-05-19 00:59:27 +0300

    minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,16 @@ import SignUp from './Components/Login/signup';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-<<<<<<< HEAD
 import Dashboard from './Components/Dashboard';
-// import Nav from './Components/nav/Nav';
-=======
-import Finals from './Components/Final/Finals';
->>>>>>> 434d889a41a913fa1b698d45c4077bd6fad244cc
 
 function App() { 
   return (
     <>
       <Router>
         <Routes>
-<<<<<<< HEAD
           <Route path='/home' exact element={<Dashboard/>}/>
           <Route path='/users/login' exact element={<SignIn/>}/>
           <Route path='/users' element={<SignUp/>}/>
-=======
-          <Route path='/home' exact element={<Home/>}/>
-          <Route path='/signin' exact element={<SignIn/>}/>
-          <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/Finals' element={<Finals/>}/>
->>>>>>> 434d889a41a913fa1b698d45c4077bd6fad244cc
         </Routes>
       </Router>
       <ToastContainer/>
